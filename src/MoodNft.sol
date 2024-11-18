2717456cfafadde0ec1cf56a3cdcabe006533271
AlesxanDer1102 <diego.orrego.t@uni.pe>
2024-11-17 22:02:53 -0500

    add a mood NFT that change of image if you want

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -5,6 +5,9 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    //error
+    error MoodNft__CantFlipMoodIfNotOwner();
+
     uint256 public s_tokenCounter;
     string private s_sadSvgImageUri;
     string private s_happySvgImageUri;
@@ -28,6 +31,21 @@ contract MoodNft is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_TokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        // only the owner of the NFT can flip the mood
+        if (
+            getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender
+        ) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+
+        if (s_TokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_TokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_TokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
